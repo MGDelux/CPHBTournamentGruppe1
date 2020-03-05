c6bae4ca424f098e9260105db38933b2bd7dd586
Tenz331 <61831296+Tenz331@users.noreply.github.com>
2020-03-05 17:43:03 +0100

    Sout

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Team team1 = new Team ("Golden Eagles", new String[]{"Ali","Mathias"});
         System.out.println(team1);
-        System.out.println("Mathias T COMMIT & PUSH TEST IF YOU SEE THIS IT WAS A SUCCESS ");
+        System.out.println("Emil Westergård ");
     }
 }
