a3e515fe00506f92391a1661e5955d1bbfedd0ff
Mathias <mathiastouboel@gmail.com>
2020-03-06 09:20:51 +0100

    Tilføjet registerTeam metode til main class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
-
+import model.Player;
 import model.Team;
 
+import java.util.ArrayList;
+
 public class Main {
+     private static ArrayList<Team> teams = new ArrayList<Team>();
 
     public static void main(String[] args) {
-        Team team1 = new Team ("Golden Eagles", new String[]{"Ali","Mathias"});
-        System.out.println(team1);
+        String teamName = "Golden memers"; // FAKE UI INPUT
+        String[] teamMembers = {"Ali","Mathias"}; // FAKE UI INPUT
+      //  Team team1 = new Team("Golden Eagles", new String[]{"Ali", "Mathias"});
+        //System.out.println(team1);
+        registerTeam(teamName,teamMembers);
+    }
+
+    private static void registerTeam(String teamName,String[] teammembers) { //
+            Team newTeam = new Team(teamName, teammembers);
+            System.out.println(newTeam); // Sout nyeste team
+            teams.add(newTeam); //Tilføjer team til vores main team arraylist.
     }
 }
 
