53641cd3511534520c91f9408a767bfecbc91297
Mathias <mathiastouboel@gmail.com>
2020-03-09 09:41:16 +0100

    8 teams tilføjet af ali

--- src/Main.java
+++ src/Main.java
@@ -8,27 +8,48 @@ public class Main {
      private static ArrayList<Match> matches= new ArrayList<Match>();
 
     public static void main(String[] args) {
+
+      //  Team team1 = new Team("Golden Eagles", new String[]{"Ali", "Mathias"});
+        //System.out.println(team1);
         String teamName = "Golden memers"; // FAKE UI INPUT
         String[] teamMembers = {"Ali","Mathias"}; // FAKE UI INPUT
         String teamName2 = "EZPZ";
         String[] teamMembers2 ={"Emil","Janus"};
-        String teamName3 = "CORONA Gang";
+        String teamName3 = "Team 3 Gang";
         String[] teamMember3={"Tess","Thor"};
         String teamName4 = "IJ ER S!ck";
         String[] teamMembers4= {"Jens","Mqens"};
-      //  Team team1 = new Team("Golden Eagles", new String[]{"Ali", "Mathias"});
-        //System.out.println(team1);
+        String teamName5 = "CphBusiness";
+        String[] teamMembers5= {"Mads","Emil"};
+        String teamName6 = "DTU";
+        String[] teamMembers6= {"Jane","Karl"};
+        String teamName7 = "Corona-virus";
+        String[] teamMembers7= {"Sara","Clara"};
+        String teamName8 = "KU";
+        String[] teamMembers8= {"Mom","Pops"};
         Team team1 = registerTeam(teamName,teamMembers);
         Team team2 = registerTeam(teamName2,teamMembers2);
         Team team3 = registerTeam(teamName3,teamMember3);
         Team team4 = registerTeam(teamName4,teamMembers4);
-        displayTeams();
+        Team team5 = registerTeam(teamName5,teamMembers5);
+        Team team6 = registerTeam(teamName6,teamMembers6);
+        Team team7 = registerTeam(teamName7,teamMembers7);
+        Team team8 = registerTeam(teamName8,teamMembers8);
+        System.out.println();
         int match1 = scheduleMatch(LocalDateTime.of(2020,06,6,12,0));
-        int match2 = scheduleMatch(LocalDateTime.of(2020,06,6,12,0));
-        int match3 = scheduleMatch(LocalDateTime.of(2020,06,6,12,0));
-        int semif1 = scheduleMatch(LocalDateTime.of(2020,06,6,12,0));
-        int semif2 = scheduleMatch(LocalDateTime.of(2020,06,6,12,0));
+        int match2 = scheduleMatch(LocalDateTime.of(2020,06,6,13,0));
+        int match3 = scheduleMatch(LocalDateTime.of(2020,06,6,14,0));
+        int match4 = scheduleMatch(LocalDateTime.of(2020,06,6,15,0));
+        int semif1 = scheduleMatch(LocalDateTime.of(2020,06,6,16,0));
+        int semif2 = scheduleMatch(LocalDateTime.of(2020,06,6,17,0));
+        int finalen = scheduleMatch(LocalDateTime.of(2020,06,6,18,0));
         updateMatch(match1,team1,team2);
+        updateMatch(match2,team3,team4);
+        updateMatch(match3,team5,team6);
+        updateMatch(match4,team7,team8);
+        displayTeams();
+
+        displayMatches();
         System.out.println(getMatchById(match1)); // for testing
         registerResult(match1,4,10);
 
